baf18d4d4921f1b840a20a85ef71e40b08207fce
Daniel Zarur <zarurdaniel@gmail.com>
2024-06-20 18:09:31 +0300

    update

--- src/script.py
+++ src/script.py
@@ -1,35 +1,31 @@
 import json
 import pandas as pd
-import glob
+from glob import glob
 
 with open('config.json', 'r') as file:
     config = json.load(file)
-settings = config['settings']
-wanted_CIK = settings["index"]
-topRisk = settings['exclude_top_risk']
-duration = settings['duration']
-CIK_path = f"./data/indexes/{wanted_CIK}.csv"
+wanted_CIK = config["index"]
+top_risk = config['exclude_top_risk']
+duration = config['duration']
+CIK_path = f"./data/indexes/{wanted_CIK}.csv" # index name insread of wanted_CIK // file_name = "{date_str}" // file_name.format(date_str=[some_parameter / value])
 
-try:
-    CIKs = pd.read_csv(CIK_path) # relavant companies to analyze from all companies in stock_price
-except FileNotFoundError:
-    raise FileNotFoundError(f"File '{CIK_path}' not found.")
+CIKs = pd.read_csv(CIK_path) # relavant companies to analyze from all companies in stock_price
 
-stock_data = glob.glob('./data/stock_prices/*.csv')
-stock_df = []
-for each in stock_data:
-    df = pd.read_csv(each, parse_dates=['date']) #maybe date is unnecessary
-    stock_df.append(df)
-stock_df = pd.concat(stock_df, ignore_index=True)
+stock_data_paths = glob('./data/stock_prices/*.csv')
+lst_dfs_stock_prices = []
+for path in stock_data_paths:
+    df_stock_price_per_date = pd.read_csv(path, parse_dates=['date']) #maybe date is unnecessary
+    lst_dfs_stock_prices.append(df_stock_price_per_date)
+df_stock_prices = pd.concat(lst_dfs_stock_prices, ignore_index=True) 
 
-risk_data= glob.glob('./data/risk_scores/*.csv')
-risk_df = []
-for each in risk_data:
+risk_data_paths= glob('./data/risk_scores/*.csv')
+lst_dfs_risk_scores = []
+for each in risk_data_paths:
     df = pd.read_csv(each, parse_dates=['date']) #maybe date is unnecessary
     risk_df.append(df)
 risk_df = pd.concat(risk_df, ignore_index=True)
 
-market_cap_data = glob.glob('./data/market_cap/*.csv')
+market_cap_data = glob('./data/market_cap/*.csv')
 market_cap_df = []
 for each in market_cap_data:
     df = pd.read_csv(each, parse_dates=['date']) #maybe date is unnecessary
@@ -40,7 +36,7 @@ relevant_risk_scores = risk_df[risk_df['CIK'].isin(CIKs['CIK'])]
 relevant_stock_prices = stock_df[stock_df['CIK'].isin(CIKs['CIK'])]
 relevant_market_cap = market_cap_df[market_cap_df['CIK'].isin(CIKs['CIK'])]
 
-top_risk_scores = relevant_risk_scores.sort_values(by='risk_score', ascending=False).head(topRisk)
+top_risk_scores = relevant_risk_scores.sort_values(by='risk_score', ascending=False).head(top_risk)
 top_risk_stock_prices = relevant_stock_prices[relevant_stock_prices.isin(top_risk_scores)]
 top_risk_market_cap = relevant_market_cap[relevant_market_cap.isin(top_risk_scores)]
 
@@ -61,12 +57,34 @@ def porftolio_price_change_contribution(weight, stock_price_change_percent):
 
 
 def entire_portfolio():
-    total_market_cap = relevant_market_cap['market_cap_at_prediction_date'].sum()
+    company_market_cap = 
+    portfolio_total_market_cap = relevant_market_cap['market_cap_at_prediction_date'].sum()
 
+    stock_price_at_prediction_date = 
+    stock_price_at_end_of_period_date = 
+    stock_price_change = (stock_price_at_end_of_period_date - stock_price_at_prediction_date) / stock_price_at_prediction_date
+    
+    weight = company_market_cap/portfolio_total_market_cap
+    portfolio_price_change_contribution = weight*stock_price_change
 
 def index_portfolio_excluding_riskiest():
-    pass
+    company_market_cap = 
+    portfolio_total_market_cap = market_cap_without_most_risky['market_cap_at_prediction_date'].sum()
 
+    stock_price_at_prediction_date = 
+    stock_price_at_end_of_period_date = 
+    stock_price_change = (stock_price_at_end_of_period_date - stock_price_at_prediction_date) / stock_price_at_prediction_date
+
+    weight = company_market_cap/portfolio_total_market_cap
+    portfolio_price_change_contribution = weight*stock_price_change
 
 def index_portfolio_riskiest_only():
-    pass
+    company_market_cap = 
+    portfolio_total_market_cap = top_risk_market_cap['market_cap_at_prediction_date'].sum()
+
+    stock_price_at_prediction_date = 
+    stock_price_at_end_of_period_date = 
+    stock_price_change = (stock_price_at_end_of_period_date - stock_price_at_prediction_date) / stock_price_at_prediction_date
+
+    weight = company_market_cap/portfolio_total_market_cap
+    portfolio_price_change_contribution = weight*stock_price_change
